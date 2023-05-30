a826454ef7670b645a54ddd0742fe8cf901583dc
stellaf <stella.fredo@gmail.com>
2023-05-30 12:02:57 +0200

    Create Contents.m

---
+++ Contents.m
@@ -0,0 +1,7 @@
+function manifest = Contents
+    manifest.Name = 'Codebeamer AMI';
+    manifest.Version = '1.1';
+    manifest.Date = datestr(now, '30-may-2023');
+    manifest.Authors = 'Inland, updated by Stella Fredo';
+    manifest.Description = 'Requirement Management Interface Add-On for MATLAB Simulink Requirements, AMI.';
+end
