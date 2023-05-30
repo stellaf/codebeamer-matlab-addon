769cae3b345a774023993cb1af78fa9f284f8bfb
stellaf <stella.fredo@gmail.com>
2023-05-30 11:50:45 +0200

    Update getDescription.m

--- intland_codebeamer_rmi/+intland/@CodeBeamer/getDescription.m
+++ intland_codebeamer_rmi/+intland/@CodeBeamer/getDescription.m
@@ -40,7 +40,7 @@ function description = getDescription(cb, item, format)
         if ~isempty(description) && ~strcmp(description, '--')
             if nargin >= 3 && strcmpi(format, 'Html') && strcmpi(item.descFormat, 'Wiki')
                 try        
-                    description = webwrite([cb.server.url '/rest/' item.uri '/wiki2html'], description, cb.server.textOptions); 
+                    description = webwrite([cb.server.url '/rest/v3/' item.uri '/wiki2html'], description, cb.server.textOptions); 
                 catch exception
                     errmsg = getReport(exception, 'basic');
                     msgbox(errmsg, 'Rendering item description to HTML failed', 'error');
